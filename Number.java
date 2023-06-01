d1b56a83c4ae96f5f385bb7784498a7f6fe76ac6
RamyaAR-1596 <134146756+RamyaAR-1596@users.noreply.github.com>
2023-06-01 22:44:40 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,27 @@
+package week1.day2;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=234;
+		if(a<0)
+		{
+			System.out.println("Number is Negative : "+a);
+		}
+		else if(a>0)
+		{
+			System.out.println("Number is Positive : "+a);
+		}
+		else if(a==0)
+		{
+			System.out.println("Number is Neutral : "+a);
+		}
+		else
+		{
+			System.out.println("number is not defined");
+		}
+
+	}
+
+}
---
+++ num.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=234;
+		if(a<0)
+		{
+			System.out.println("Number is Negative");
+		}
+		else if(a>0)
+		{
+			System.out.println("Number is Positive");
+		}
+		else if(a==0)
+		{
+			System.out.println("Number is Neutral");
+		}
+		else
+		{
+			System.out.println("number is not defined");
+		}
+	}
+
+}
